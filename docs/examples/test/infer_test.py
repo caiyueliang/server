44306139e108fe98019aaeda491df8b8f8656e53
caiyueliang <393900414@qq.com>
2023-03-20 17:20:05 +0800

    update

--- docs/examples/test/infer_test.py
+++ docs/examples/test/infer_test.py
@@ -1,7 +1,28 @@
 import requests
+import argparse
+import logging
+
+logger = logging.getLogger()
+
+
+def parse_argvs():
+    parser = argparse.ArgumentParser(description='asr client')
+    parser.add_argument("--processes", help="processes num", type=int, default=1)
+    parser.add_argument("--threads", help="threads num", type=int, default=1)
+    parser.add_argument("--times", help="test times", type=int, default=1)
+    parser.add_argument("--server_address", help="server_address", type=str,
+                        default="https://aigc.wair.ac.cn")
+
+    args = parser.parse_args()
+    logger.info('[args] {}'.format(args))
+
+    return parser, args
 
 
 if __name__ == "__main__":
+    parser, args = parse_argvs()
+    logger.warning("[main] server_address: {}".format(args.server_address))
+
     request_data = {
         "inputs": [{
             "name": "input__0",
