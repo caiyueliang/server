296a5045fc6152b6a1a50e8b2b034708662797f8
caiyueliang <393900414@qq.com>
2023-03-20 17:44:55 +0800

    update

--- docs/examples/test/infer_test.py
+++ docs/examples/test/infer_test.py
@@ -12,8 +12,8 @@ def parse_argvs():
     parser.add_argument("--processes", help="processes num", type=int, default=1)
     parser.add_argument("--threads", help="threads num", type=int, default=1)
     parser.add_argument("--times", help="test times", type=int, default=1)
-    parser.add_argument("--server_address", help="server_address", type=str,
-                        default="https://aigc.wair.ac.cn")
+    # parser.add_argument("--url", type=str, default="http://localhost:8000/v2/models/fc_model_pt/versions/1/infer")
+    parser.add_argument("--url", type=str, default="http://localhost:8000/v2/models/simple/versions/1/infer")
 
     args = parser.parse_args()
     logger.warning('[args] {}'.format(args))
@@ -25,17 +25,34 @@ if __name__ == "__main__":
     parser, args = parse_argvs()
     logger.warning("[main] server_address: {}".format(args.server_address))
 
+    # request_data = {
+    #     "inputs": [{
+    #         "name": "input__0",
+    #         "shape": [2, 3],
+    #         "datatype": "INT64",
+    #         "data": [[1, 2, 3], [4, 5, 6]]
+    #     }],
+    #     "outputs": [{"name": "output__0"}, {"name": "output__1"}]
+    # }
     request_data = {
-        "inputs": [{
-            "name": "input__0",
-            "shape": [2, 3],
-            "datatype": "INT64",
-            "data": [[1, 2, 3], [4, 5, 6]]
-        }],
-        "outputs": [{"name": "output__0"}, {"name": "output__1"}]
+        "inputs": [
+            {
+                "name": "input0",
+                "shape": [16],
+                "datatype": "INT32",
+                "data": [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]]
+            },
+            {
+                "name": "input1",
+                "shape": [16],
+                "datatype": "INT32",
+                "data": [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]]
+            }
+        ],
+        "outputs": [{"name": "output0"}, {"name": "output1"}]
     }
 
-    response = requests.post(url="http://localhost:8000/v2/models/fc_model_pt/versions/1/infer", json=request_data)
+    response = requests.post(url=args.url, json=request_data)
     logger.warning("response: {}, dict: {}".format(response, response.__dict__))
 
     if response.status_code == 200:
