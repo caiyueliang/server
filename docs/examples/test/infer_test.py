b79bf54c7ecd55bb2191b75e8aff087962fb91a1
caiyueliang <393900414@qq.com>
2023-03-20 16:57:56 +0800

    update

---
+++ docs/examples/test/infer_test.py
@@ -0,0 +1,16 @@
+import requests
+
+
+if __name__ == "__main__":
+    request_data = {
+        "inputs": [{
+            "name": "input__0",
+            "shape": [2, 3],
+            "datatype": "INT64",
+            "data": [[1, 2, 3],[4,5,6]]
+        }],
+        "outputs": [{"name": "output__0"}, {"name": "output__1"}]
+    }
+
+    res = requests.post(url="http://localhost:8000/v2/models/fc_model_pt/versions/1/infer", json=request_data).json()
+    print(res)
